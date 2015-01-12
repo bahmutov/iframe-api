9b52e336fa22a66f6b742fe7494393e4cf7595e1
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2015-01-12 14:39:05 -0500

    Updates

--- src/external-api.js
+++ src/external-api.js
@@ -2,55 +2,77 @@
 // installs api object that can be used to execute code
 // in iframe's context
 
-// this (parent) website API to be called from the iframe
-var parentApi = {
-  message: function () {
-    /* eslint no-alert:0, no-undef:0 */
-    alert(Array.prototype.slice.call(arguments, 0).join(' '));
-  }
-};
+(function initIframeApi() {
 
-function externalApi() {
-  function send(cmd) {
-    parent.postMessage({
-      cmd: cmd,
-      args: Array.prototype.slice.call(arguments, 1)
-    }, '*');
-  }
+  /* global console */
 
-  // the api should match closely parentApi object
-  var api = {
-    message: send.bind(null, 'message')
-  };
-  return api;
-}
-
-// receives message (possibly from the iframe)
-window.onmessage = function (event) {
-  console.log('parent received', event.data);
-
-  function reviveApi(options) {
-    console.log('received iframe API, MD5', options.md5);
-    // you can verify that md5 of the src matches passed from
-    /* jshint -W061 */
-    /* eslint no-eval:0 */
-    window.iframeApi = eval('(' + options.text + ')(event.source, options.apiMethodNames, options.apiMethodHelps)');
-    console.log('You can now access iframe via iframeApi object');
+  function makeExternalApi(methodNames) {
+    function send(cmd) {
+      // parent is the window element from iframe
+      parent.postMessage({
+        cmd: cmd,
+        args: Array.prototype.slice.call(arguments, 1)
+      }, '*');
+    }
+    var api = {};
+    methodNames.forEach(function (name) {
+      api[name] = send.bind(null, name);
+    });
+    return api;
   }
 
-  function sendExternalApi() {
-    window.iframeApi.api(externalApi.toString());
+  function iframeApi(externalApi, callback, options) {
+    if (typeof externalApi === 'function') {
+      options = callback;
+      callback = externalApi;
+      externalApi = null;
+    }
+    options = options || {};
+
+    var frameApi;
+
+    // receives message (possibly from the iframe)
+    function processMessage(event) {
+      console.log('parent received', event.data);
+
+      function reviveApi(options) {
+        console.log('received iframe API, MD5', options.md5);
+        // you can verify that md5 of the src matches passed from
+        /* jshint -W061 */
+        /* eslint no-eval:0 */
+        return eval('(' + options.text + ')(event.source, options.apiMethodNames, options.apiMethodHelps)');
+      }
+
+      function sendExternalApi(frameApi) {
+        if (externalApi) {
+          console.log('sending external api back to the frame');
+          frameApi.api({
+            source: makeExternalApi.toString(),
+            methodNames: Object.keys(externalApi)
+          });
+        }
+      }
+
+      if (event.data.cmd === 'api') {
+        frameApi = reviveApi(event.data);
+        sendExternalApi(frameApi);
+        setTimeout(function () {
+          callback(null, frameApi);
+        }, 0);
+        return;
+      }
+
+      if (externalApi) {
+        var method = externalApi[event.data.cmd];
+        if (typeof method === 'function') {
+          // parent has a method iframe is trying to call
+          method.apply(externalApi, event.data.args);
+        }
+      }
+    }
+
+    window.onmessage = processMessage;
   }
 
-  if (event.data.cmd === 'api') {
-    reviveApi(event.data);
-    sendExternalApi();
-    return;
-  }
-
-  var method = parentApi[event.data.cmd];
-  if (typeof method === 'function') {
-    // parent has a method iframe is trying to call
-    method.apply(parentApi, event.data.args);
-  }
-};
+  window.iframeApi = iframeApi;
+}());
