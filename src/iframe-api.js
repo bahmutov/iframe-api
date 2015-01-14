d6434bc9a9b7dc783e28ce35de58cab945eb36d2
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2015-01-14 12:07:24 -0500

    Original iframe api call returns a promise, fixed #11

--- src/iframe-api.js
+++ src/iframe-api.js
@@ -1,61 +1,56 @@
 // to be run from <iframe src="..." ...></iframe>
 
+require('es6-promise').polyfill();
+
 function isIframed() {
   return parent !== window;
 }
 
-var la = require('./la');
 var apiMethods = require('./revive-api');
-var figureOutOptions = require('./figure-out-options');
 
-var iframeApi = function iframeApi(myApi, cb, userOptions) {
-  var params = figureOutOptions(myApi, cb, userOptions);
+var iframeApi = function iframeApi(myApi, userOptions) {
+  var params = {
+    myApi: myApi,
+    options: userOptions || {}
+  };
 
-  la(typeof params.callback === 'function', 'need callback function', params);
+  return new Promise(function (resolve, reject) {
 
-  function processMessage(e) {
-    if (!e.data || !e.data.cmd) {
-      console.error('invalid message received by the iframe API', e.data);
-      return;
-    }
-
-    if (e.data.cmd === 'api') {
-      try {
-        var api = apiMethods.reviveApi(params.options, e.data, parent);
-
-        if (!isIframed() && params.myApi) {
-          console.log('sending external api back to the iframe');
-          apiMethods.send(params.myApi, e.source);
+    function processMessage(e) {
+      if (!e.data || !e.data.cmd) {
+        console.error('invalid message received by the iframe API', e.data);
+        return;
+      }
+      if (e.data.cmd === 'api') {
+        try {
+          var api = apiMethods.reviveApi(params.options, e.data, e.source);
+          if (!isIframed() && params.myApi) {
+            console.log('sending external api back to the iframe');
+            apiMethods.send(params.myApi, e.source);
+          }
+          resolve(api);
+        } catch (err) {
+          reject(err);
         }
-
-        setTimeout(function () {
-          params.callback(null, api);
-        }, 0);
-      } catch (err) {
-        setTimeout(function () {
-          params.callback(err);
-        }, 0);
+        return;
       }
 
-      return;
-    }
-
-    if (params.myApi) {
-      var method = params.myApi[e.data.cmd];
-      if (typeof method === 'function') {
-        var args = Array.isArray(e.data.args) ? e.data.args : [];
-        method.apply(params.myApi, args);
-      } else {
-        console.log('unknown command', e.data.cmd, 'from the parent', e.data.cmd);
+      if (params.myApi) {
+        var method = params.myApi[e.data.cmd];
+        if (typeof method === 'function') {
+          var args = Array.isArray(e.data.args) ? e.data.args : [];
+          method.apply(params.myApi, args);
+        } else {
+          console.log('unknown command', e.data.cmd, 'from the parent', e.data.cmd);
+        }
       }
     }
-  }
+    window.onmessage = processMessage;
 
-  window.onmessage = processMessage;
-
-  if (isIframed() && params.myApi) {
-    apiMethods.send(params.myApi, parent);
-  }
+    if (isIframed() && params.myApi) {
+      apiMethods.send(params.myApi, parent);
+    }
+  });
 };
 
 module.exports = iframeApi;
